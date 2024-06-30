68ebc9251813b8ac5503885c0b4f135f52fb0c12
SIMON\范峻川 <junchuan1019.fan@gmail.com>
2024-06-30 10:01:02 +0800

    Update fragment

--- app/src/main/java/com/simon/androiddemo002/MainActivity.java
+++ app/src/main/java/com/simon/androiddemo002/MainActivity.java
@@ -7,28 +7,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int i=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setButton();
     }
-    private void setButton()
-    {
-        Button button = findViewById(R.id.button_id);
-        Button button1 = findViewById(R.id.button2_id);
-        TextView textView = findViewById(R.id.textView);
-        button.setOnClickListener(v -> {
-            i++;
-            String data= String.valueOf(i);
-            textView.setText(data);
-        });
-        button1.setOnClickListener(v -> {
-            i--;
-            String data=String.valueOf(i);
-            textView.setText(data);
-        });
-    }
+
+
     //onCreate， onStart, onResume,  onPause, onStop, onRestart, onDestroy
 }
