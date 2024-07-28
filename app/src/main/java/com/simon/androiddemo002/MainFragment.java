d9af9829f64e7035a08bda49f576c3071c912b24
SIMON\范峻川 <junchuan1019.fan@gmail.com>
2024-07-28 16:18:52 +0800

    Update some image and some emails

--- app/src/main/java/com/simon/androiddemo002/MainFragment.java
+++ app/src/main/java/com/simon/androiddemo002/MainFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,5 +15,17 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main,container,false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setButton(view);
+        //LOL LOL
+    }
+    protected void setButton(View view){
+        Button button = view.findViewById(R.id.next_id);
+        button.setOnClickListener(v -> getParentFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,new Fragment2()).commit());
     }
 }
